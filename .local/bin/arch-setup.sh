9a293d635180fb19baf7aa430b75fa96d21a9cd9
Giovane Boaviagem Ribeiro <giovanebribeiro@gmail.com>
2025-06-18 23:03:55 -0300

    removing old configs

--- .local/bin/arch-setup.sh
+++ .local/bin/arch-setup.sh
@@ -14,10 +14,17 @@ echo "Install arch dependencies"
 # * blueman: GUI + applet for bluetooth service 
 # * gvim : VIm with clipboard enabled
 # * meld: winmerge-like tool 
-pgrep -x pacman > /dev/null && sudo pacman -Sy llvm-libs mesa rust alacritty bspwm sxhkd polybar feh autorandr xorg-xrandr unzip arandr blueman gvim meld
+# * powerline: better fonts and emojis
+# * tmux: terminal sessions
+pgrep -x pacman > /dev/null && sudo pacman -Sy llvm-libs mesa rust alacritty bspwm sxhkd polybar feh autorandr xorg-xrandr unzip arandr blueman gvim meld powerline tmux
 
 echo "Install Vundle (vim plugin) and install vim plugins"
 [ ! -d $HOME/.vim/sessions ] && mkdir -p $HOME/.vim/sessions
 [ ! -d $HOME/.vim/bundle ] && mkdir -p $HOME/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim/
 vim +PluginInstall +qall
+
+echo "Install oh-my-tmux"
+[ ! -d $HOME/.config/tmux ] && mkdir -p $HOME/.config/tmux
+git clone https://github.com/gpakosz/.tmux $HOME/.config/tmux/
+ln -sf $HOME/.config/tmux/.tmux.conf $HOME/.tmux.conf
--- .local/bin/arch-update.sh
+++ .local/bin/arch-update.sh
@@ -23,3 +23,6 @@ done
 echo "## Atualizando plugins do VIm"
 vim +PluginUpdate +qall
 
+echo "## Atualizando oh-my-tmux"
+cd $HOME/.config/tmux
+git pull
