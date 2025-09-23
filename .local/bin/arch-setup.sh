e3ef7108ed04a43043f76a2ba8f9ca58d33fd66a
Giovane Boaviagem Ribeiro <giovanebribeiro@gmail.com>
2025-09-23 17:54:06 -0300

    feat: add aws cli on arch setup

--- .local/bin/arch-setup.sh
+++ .local/bin/arch-setup.sh
@@ -78,3 +78,10 @@ sudo systemctl enable cronie
 
 echo "Install devcontainer cli"
 npm -g @devcontainers/cli
+
+echo "Install aws cli"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf ./aws
+rm awscliv2.zip
