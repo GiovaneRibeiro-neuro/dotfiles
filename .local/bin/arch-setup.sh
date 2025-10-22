d4cfc77b3da69995d9160c35f9142e6ed8c40cbe
Giovane Boaviagem Ribeiro <giovanebribeiro@gmail.com>
2025-10-22 12:55:51 -0300

    add sdkman and nvm in load scripts

--- .local/bin/arch-setup.sh
+++ .local/bin/arch-setup.sh
@@ -76,12 +76,28 @@ systemctl --user restart gnome-keyring-daemon.service
 sudo systemctl start cronie
 sudo systemctl enable cronie
 
-echo "Install devcontainer cli"
-npm -g @devcontainers/cli
+echo "Install frameworks"
+nvm install 22
+sdk install java
 
-echo "Install aws cli"
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-rm -rf ./aws
-rm awscliv2.zip
+
+#echo "Install devcontainer cli"
+#npm -g @devcontainers/cli
+#
+#echo "Install aws cli"
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#unzip awscliv2.zip
+#sudo ./aws/install
+#rm -rf ./aws
+#rm awscliv2.zip
+
+echo "Install Github CLI"
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
