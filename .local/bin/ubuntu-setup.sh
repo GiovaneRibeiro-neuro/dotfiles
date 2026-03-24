a65fda964899ae5cf4c71af27dd6080be666c061
Giovane Boaviagem Ribeiro <giovanebribeiro@gmail.com>
2026-03-24 15:21:23 -0300

    add common-* scripts

--- .local/bin/ubuntu-setup.sh
+++ .local/bin/ubuntu-setup.sh
@@ -30,36 +30,6 @@ sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
 
-echo "Install kind"
-# For AMD64 / x86_64
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.31.0/kind-linux-amd64
-# For ARM64
-[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.31.0/kind-linux-arm64
-chmod +x ./kind
-sudo mv ./kind /usr/local/bin/kind
-
-
-echo "Install Vundle (vim plugin) and install vim plugins"
-[ ! -d $HOME/.vim/sessions ] && mkdir -p $HOME/.vim/sessions
-[ ! -d $HOME/.vim/bundle ] && mkdir -p $HOME/.vim/bundle
-git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim/
-vim +PluginInstall +qall
-
-echo "Install oh-my-tmux"
-[ ! -d $HOME/.config/tmux ] && mkdir -p $HOME/.config/tmux
-git clone https://github.com/gpakosz/.tmux $HOME/.config/tmux/
-ln -sf $HOME/.config/tmux/.tmux.conf $HOME/.tmux.conf
-
-echo "Install frameworks"
-nvm install 22
-sdk install java
-sdk install maven
-sudo apt install golang-go
-curl -sSfL https://golangci-lint.run/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.9.0
-
-echo "Install other tools"
-npm install -g @anthropic-ai/claude-code
-
 echo "Install Github CLI"
 (type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
 	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
